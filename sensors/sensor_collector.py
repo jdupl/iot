d9155a6b36d8e5bc322bc29f37bd9b144c7f499d
Justin Duplessis <drfoliberg@gmail.com>
2016-05-17 15:48:02 -0400

    Move header code

--- sensors/sensor_collector.py
+++ sensors/sensor_collector.py
@@ -9,6 +9,8 @@ def collect_data():
     collector_instance = DHT11Collector(14)
     csv_path = 'sensor_1.csv'
 
+    write_csv_headers(csv_path, collector_instance)
+
     while True:
         r = collector_instance.get_data()
         if r:
@@ -17,15 +19,18 @@ def collect_data():
 
 
 def write(result, csv_path, collector_instance):
-    if not os.path.exists(csv_path):
-        with open(csv_path, 'w') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['Unix epoch (s)',
-                            *collector_instance.get_csv_headers()])
-
     with open(csv_path, 'a') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([time.time(), *result])
+
+
+def write_csv_headers(csv_path, collector_instance):
+    if os.path.exists(csv_path):
+        return
+    with open(csv_path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Unix epoch (s)',
+                        *collector_instance.get_csv_headers()])
 
 if __name__ == '__main__':
     collect_data()
