c3652069a1259f567c436ef688de3d8d074711cd
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-05-20 12:16:30 -0400

    flask-sqlalchemy

--- sensor-hub/app.py
+++ sensor-hub/app.py
@@ -1,8 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SQLALCHEMRY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer)
+
+    def __init__(self, value):
+        self.value = value
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    data = request.data
+    record = Record(data)
+    db.session.add(record)
+    db.session.commit()
     return 'Hello World!'
 
 if __name__ == '__main__':
--- sensor-hub/test.py
+++ sensor-hub/test.py
@@ -1,17 +1,34 @@
 import os
-import app
+from app import app, db, Record
 import unittest
 import tempfile
 
 class HelloWorld(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.app.test_client()
+        self.app = app.test_client()
+        self.db = db
+        self.db.session.close()
+        self.db.drop_all()
+        self.db.create_all()
 
     def test_hello_world(self):
         res = self.app.get('/')
         data = res.data.decode('utf-8')
         assert data == 'Hello World!'
+
+    def test_create_record(self):
+        record = Record(1024)
+        self.db.session.add(record)
+        self.db.session.commit()
+        records = Record.query.all()
+        assert len(records) == 1
+
+    def test_single_message(self):
+        res = self.app.post('/', data='1024')
+        assert res.status_code == 200
+        records = Record.query.all()
+        assert len(records) == 1
 
 if __name__ == '__main__':
     unittest.main()
