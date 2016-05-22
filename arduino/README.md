2628b85151bd2222e5c6c08adc25df5c2896f9ef
Justin Duplessis <drfoliberg@gmail.com>
2016-05-22 12:32:21 -0400

    Build without IDE

---
+++ arduino/README.md
@@ -0,0 +1,17 @@
+# Plant watcher
+
+Collects soil humidity of plants and sends data to an external server via ESP01.
+
+
+### Dependencies
+
+`sudo apt-get install arduino-mk`
+
+
+### Uploading on an Arduino
+
+`make upload`
+
+Change the Makefile to your needs:
+
+ (`make show_boards` for complete list).
--- arduino/collector.ino
+++ arduino/collector.ino
@@ -7,6 +7,29 @@ String SSID = "wifi SSID";
 String password = "wifi password";
 String serverIp = "(HTTP) Server ip or name";
 
+bool execOnESP(String cmd, String expectedRes, unsigned long timeout) {
+    unsigned long tExpire = millis() + timeout;
+    String response = "";
+
+    softSerial.println(cmd);
+
+    while (millis() < tExpire) {
+        char c = softSerial.read();
+        if (c >= 0) {
+            response += String(c);
+            if (response.indexOf(expectedRes) > 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void reset() {
+    execOnESP("AT+RST", "ready", 20000);
+    execOnESP("AT+CWMODE=1", "OK", 5000);
+    execOnESP("AT+CWJAP=\"" + SSID + "\",\"" + password + "\"", "OK" , 15000);
+}
 
 void setup() {
     Serial.begin(9600);
@@ -23,13 +46,6 @@ void setup() {
     execOnESP("AT+CWJAP=\"" + SSID + "\",\"" + password + "\"", "OK" , 15000);
 }
 
-void loop() {
-    if (!update()) {
-        reset();
-    }
-    delay(3000);
-}
-
 bool update() {
     if (!execOnESP("AT+CIPSTART=\"TCP\",\"" + serverIp + "\",80", "OK", 5000))
         return false;
@@ -44,27 +60,9 @@ bool update() {
         return false;
 }
 
-void reset() {
-    execOnESP("AT+RST", "ready", 20000);
-    execOnESP("AT+CWMODE=1", "OK", 5000);
-    execOnESP("AT+CWJAP=\"" + SSID + "\",\"" + password + "\"", "OK" , 15000);
-}
-
-bool execOnESP(String cmd, String expectedRes, unsigned long timeout) {
-    unsigned long tExpire = millis() + timeout;
-    String response = "";
-
-    softSerial.println(cmd);
-
-    while (millis() < tExpire) {
-        char c = softSerial.read();
-        if (c >= 0) {
-            response += String(c);
-            if (response.indexOf(expectedRes) > 0) {
-                return true;
-            }
-        }
+void loop() {
+    if (!update()) {
+        reset();
     }
-
-    return false;
+    delay(3000);
 }
