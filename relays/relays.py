11c81ce6041076f2f2fc172fcfcb71bb42c3cd2d
Justin Duplessis <drfoliberg@gmail.com>
2016-05-31 00:29:45 -0400

    WIP

--- relays/relays.py
+++ relays/relays.py
@@ -1,25 +1,31 @@
 import time
 import datetime as dt
 
-
-open_time = dt.time(5, 0, 0)
-close_time = dt.time(23, 55, 0)
-light_pins = [23, 24]  # GPIO pins of the relay (BCM)
-
 GPIO = None
 
 
 class Schedule():
     """Represents open and close events for relay pins."""
+
+    __curr_state = 'off'
+
     def __init__(self, pins, open_times, close_times):
         self.pins = pins
         self.open_times = open_times
         self.close_times = close_times
 
-    def get_future_events(self, now=dt.datetime.now()):
+    def get_latest_event(self, now=dt.datetime.now()):
+        past = sorted(self._get_past_events(now), reverse=True)
+        return past[0]
+
+    def get_next_event(self, now=dt.datetime.now()):
+        future = sorted(self._get_future_events(now))
+        return future[0]
+
+    def _get_future_events(self, now):
         return self._get_events(True, now)
 
-    def get_past_events(self, now=dt.datetime.now()):
+    def _get_past_events(self, now):
         return self._get_events(False, now)
 
     def _get_events(self, future, now):
@@ -44,10 +50,6 @@ class Schedule():
 
             events.append((event_time + dt.timedelta(days=delta), event[1]))
         return events
-
-
-def load_config():
-    return True
 
 
 def get_events(time_array, future):
@@ -77,13 +79,14 @@ def get_next_event(time_array):
     return future[0]
 
 
-def lights(state_str):
+def set_relay(pins, state_str):
     # Yes, relay on needs gpio value 0
-    gpio_val = 1 if state_str == 'off' else 0
+    for pin in pins:
+        gpio_val = 1 if state_str == 'off' else 0
 
-    for pin in light_pins:
-        print('Pin %d is now %s (%d)' % (pin, state_str, gpio_val))
-        GPIO.output(pin, gpio_val)
+        for pin in pins:
+            print('Pin %d is now %s (%d)' % (pin, state_str, gpio_val))
+            GPIO.output(pin, gpio_val)
 
 
 def setup_light_pins(pins):
@@ -92,34 +95,62 @@ def setup_light_pins(pins):
         GPIO.setup(pin, GPIO.OUT, initial=1)
 
 
-def control_lights():
-    __curr_state = 'off'
+def control_and_sleep(schedules):
+    now = dt.datetime.now()
+    next_event_max = 10
+    print('Currently %s.' % str(now))
+
+    for schedule in schedules:
+        wanted_state = schedule.get_latest_event()[1]
+
+        if schedule.__curr_state != wanted_state:
+            set_relay(schedule.pins, wanted_state)
+            schedule.__curr_state = wanted_state
+
+        next_event = schedule.get_next_event()[0]
+
+        if next_event > next_event_max:
+            next_event_max = next_event
+
+    next_change_in = (next_event_max - now).total_seconds()
+    print('Next_change_in %s' % next_change_in)
+    time.sleep(next_change_in)
+
+
+def control_relays(schedules):
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     setup_light_pins(light_pins)
 
     while True:
-        now = dt.datetime.now()
-        print('Currently %s.' % str(now))
+        control_and_sleep()
+        time.sleep(1)  # Overflow next schedule
 
-        wanted_state = get_latest_event([open_time, close_time])[1]
 
-        if __curr_state != wanted_state:
-            print('Bringing lights %s.' % wanted_state)
-            lights(wanted_state)
-            __curr_state = wanted_state
+def each(hours, start_at=dt.time(0, 0, 0), max_iterations=24):
+    times = []
 
-        next_event = get_next_event([open_time, close_time])[0]
-        next_change_in = (next_event - now).total_seconds()
-        print('Next_change_in %s' % next_change_in)
-
-        sleep_until = next_change_in + 1
-        time.sleep(sleep_until)
+    time = start_at
+    while time.hour + (hours) * (len(times) + 1) < 24 and \
+            (max_iterations > 0 and max_iterations > len(times)):
+        t = dt.time(time.hour + (hours) * (len(times) + 1), time.minute, time.second)
+        times.append(t)
+        print(t)
+    return times
 
 if __name__ == '__main__':
     import RPi.GPIO as GPIO
     try:
-        control_lights()
+        light_pins = [23, 24]  # GPIO pins of the relay (BCM)
+        light_schedule = Schedule(light_pins,
+                                  [dt.time(5, 0, 0)], [dt.time(23, 55, 0)])
+        fan_schedule = Schedule([22],
+                                each(3, start_at=dt.time(5, 30, 0),
+                                max_iterations=6),
+                                each(3, start_at=dt.time(6, 0, 0),
+                                max_iterations=6))
+
+        control_relays([light_schedule, fan_schedule])
     except KeyboardInterrupt:
         print('Got KeyboardInterrupt.')
     except Exception as e:
--- relays/test.py
+++ relays/test.py
@@ -1,16 +1,22 @@
 import datetime as dt
 import unittest
 
+import relays
 from relays import Schedule
 
 
-class RelayTest(unittest.TestCase):
+class UtilsTest(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_each(self):
+        fan_schedule = Schedule([22],
+                                relays.each(1, start_at=dt.time(5, 30, 0)),
+                                relays.each(3, start_at=dt.time(6, 0, 0)))
+        print(fan_schedule.get_next_event())
 
-    def tearDown(self):
-        pass
+        # self.assertEqual(fan_schedule.get_next_event())
+
+
+class ScheduleTest(unittest.TestCase):
 
     def test_get_events_from_simple_schedule_morning(self):
         # It's 4h
@@ -19,16 +25,16 @@ class RelayTest(unittest.TestCase):
         close_time = dt.time(23, 55, 0)
         s = Schedule(14, [open_time], [close_time])
 
-        self.assertEqual(len(s.get_future_events(now=now)), 4)
-        self.assertEqual(s.get_future_events(now=now), [
+        self.assertEqual(len(s._get_future_events(now=now)), 4)
+        self.assertEqual(s._get_future_events(now=now), [
             (dt.datetime(2016, 5, 30, 5, 0), 'on'),
             (dt.datetime(2016, 5, 31, 5, 0), 'on'),
             (dt.datetime(2016, 5, 30, 23, 55), 'off'),
             (dt.datetime(2016, 5, 31, 23, 55), 'off'),
         ])
 
-        self.assertEqual(len(s.get_past_events(now=now)), 2)
-        self.assertEqual(s.get_past_events(now=now), [
+        self.assertEqual(len(s._get_past_events(now=now)), 2)
+        self.assertEqual(s._get_past_events(now=now), [
             (dt.datetime(2016, 5, 29, 5, 0), 'on'),
             (dt.datetime(2016, 5, 29, 23, 55), 'off'),
         ])
@@ -40,14 +46,14 @@ class RelayTest(unittest.TestCase):
         close_time = dt.time(23, 55, 0)
         s = Schedule(14, [open_time], [close_time])
 
-        self.assertEqual(len(s.get_future_events(now=now)), 3)
-        self.assertEqual(s.get_future_events(now=now), [
+        self.assertEqual(len(s._get_future_events(now=now)), 3)
+        self.assertEqual(s._get_future_events(now=now), [
             (dt.datetime(2016, 5, 31, 5, 0), 'on'),
             (dt.datetime(2016, 5, 30, 23, 55), 'off'),
             (dt.datetime(2016, 5, 31, 23, 55), 'off'),
         ])
-        self.assertEqual(len(s.get_past_events(now=now)), 3)
-        self.assertEqual(s.get_past_events(now=now), [
+        self.assertEqual(len(s._get_past_events(now=now)), 3)
+        self.assertEqual(s._get_past_events(now=now), [
             (dt.datetime(2016, 5, 30, 5, 0), 'on'),
             (dt.datetime(2016, 5, 29, 5, 0), 'on'),
             (dt.datetime(2016, 5, 29, 23, 55), 'off'),
@@ -60,13 +66,13 @@ class RelayTest(unittest.TestCase):
         close_time = dt.time(23, 0, 0)
         s = Schedule(14, [open_time], [close_time])
 
-        self.assertEqual(len(s.get_future_events(now=now)), 2)
-        self.assertEqual(s.get_future_events(now=now), [
+        self.assertEqual(len(s._get_future_events(now=now)), 2)
+        self.assertEqual(s._get_future_events(now=now), [
             (dt.datetime(2016, 5, 31, 5, 0), 'on'),
             (dt.datetime(2016, 5, 31, 23, 0), 'off'),
         ])
-        self.assertEqual(len(s.get_past_events(now=now)), 4)
-        self.assertEqual(s.get_past_events(now=now), [
+        self.assertEqual(len(s._get_past_events(now=now)), 4)
+        self.assertEqual(s._get_past_events(now=now), [
             (dt.datetime(2016, 5, 30, 5, 0), 'on'),
             (dt.datetime(2016, 5, 29, 5, 0), 'on'),
             (dt.datetime(2016, 5, 30, 23, 0), 'off'),
@@ -79,8 +85,8 @@ class RelayTest(unittest.TestCase):
         s = Schedule(14, [dt.time(5, 0, 0), dt.time(15, 0, 0)],
                      [dt.time(10, 0, 0), dt.time(19, 0, 0)])
 
-        self.assertEqual(len(s.get_future_events(now=now)), 8)
-        self.assertEqual(s.get_future_events(now=now), [
+        self.assertEqual(len(s._get_future_events(now=now)), 8)
+        self.assertEqual(s._get_future_events(now=now), [
             (dt.datetime(2016, 5, 30, 5, 0), 'on'),
             (dt.datetime(2016, 5, 31, 5, 0), 'on'),
             (dt.datetime(2016, 5, 30, 15, 0), 'on'),
@@ -91,13 +97,25 @@ class RelayTest(unittest.TestCase):
             (dt.datetime(2016, 5, 31, 19, 0), 'off'),
         ])
 
-        self.assertEqual(len(s.get_past_events(now=now)), 4)
-        self.assertEqual(s.get_past_events(now=now), [
+        self.assertEqual(len(s._get_past_events(now=now)), 4)
+        self.assertEqual(s._get_past_events(now=now), [
             (dt.datetime(2016, 5, 29, 5, 0), 'on'),
             (dt.datetime(2016, 5, 29, 15, 0), 'on'),
             (dt.datetime(2016, 5, 29, 10, 0), 'off'),
             (dt.datetime(2016, 5, 29, 19, 0), 'off'),
         ])
+
+    def test_get_latest_event_from_multiple_schedule(self):
+        # It's 4h
+        now = dt.datetime.fromtimestamp(1464595200)
+        s = Schedule(14, [dt.time(5, 0, 0), dt.time(15, 0, 0)],
+                     [dt.time(10, 0, 0), dt.time(19, 0, 0)])
+
+        self.assertEqual(s.get_latest_event(now=now),
+                         (dt.datetime(2016, 5, 29, 19, 0), 'off'))
+
+        self.assertEqual(s.get_next_event(now=now),
+                         (dt.datetime(2016, 5, 30, 5, 0), 'on'))
 
 if __name__ == '__main__':
     unittest.main()
