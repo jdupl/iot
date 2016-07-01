edd89e138ceb6f62691245a456581bc5e34a4597
Justin Duplessis <drfoliberg@gmail.com>
2016-07-01 13:22:12 -0400

    Add colors to headers

--- sensor-hub/public/controllers/index.js
+++ sensor-hub/public/controllers/index.js
@@ -11,10 +11,25 @@ controllers.controller('IndexController', function($scope, $http) {
       $http.get('/api/records/latest')
           .success(function(data) {
               $scope.records = data.records;
+              for (var i = 0; i < $scope.records.length; i++) {
+                $scope.records[i].header = 'panel-' + getHeaderForValue($scope.records[i].value);
+              }
           })
           .error(function(err, status) {
               console.log(err, status);
           });
     };
   $scope.refresh();
+
+  function getHeaderForValue(val) {
+    if (val > 920) {
+      return 'primary';
+    } else if (val > 800 || val < 300) {
+      return 'danger';
+    } else if (val > 600) {
+      return 'success';
+    } else {
+      return 'warning';
+    }
+  }
 });
