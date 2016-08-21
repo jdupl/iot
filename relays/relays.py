caa09993e7c1d833261937fb94521d264fad4042
Justin Duplessis <drfoliberg@gmail.com>
2016-08-20 20:31:01 -0400

    Add bloom sche

--- relays/relays.py
+++ relays/relays.py
@@ -135,9 +135,12 @@ def each(hours, start_at=dt.time(0, 0, 0), max_iterations=24):
 if __name__ == '__main__':
     import RPi.GPIO as GPIO
     try:
-        light_pins = [23, 24, 25, 27]  # GPIO pins of the lights (BCM)
-        light_schedule = Schedule(light_pins,
-                                  [dt.time(5, 0, 0)], [dt.time(23, 55, 0)])
+        veg_light_pins = [24]  # GPIO pins of the lights (BCM)
+        veg_light_schedule = Schedule(veg_light_pins,
+                                      [dt.time(5, 0, 0)], [dt.time(23, 55, 0)])
+        bloom_light_pins = [23]  # GPIO pins of the lights (BCM)
+        bloom_light_schedule = Schedule(bloom_light_pins, [dt.time(8, 0, 0)],
+                                        [dt.time(20, 0, 0)])
         fan_schedule = Schedule([22],  # GPIO pins of the fans (BCM)
                                 each(1, start_at=dt.time(5, 50, 0)),
                                 each(1, start_at=dt.time(5, 55, 0)))
