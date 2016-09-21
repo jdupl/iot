c4ca890798a854c9a9c59e4bbc0f3bf03aa75010
Justin Duplessis <drfoliberg@gmail.com>
2016-09-20 21:20:30 -0400

    API: populate dht11 data

--- sensor-hub/populate_dev_db.py
+++ sensor-hub/populate_dev_db.py
@@ -3,7 +3,9 @@ import os
 import csv
 
 from datetime import datetime as dt
-from sensor_hub import db_session, Record, setup
+from sensor_hub import setup
+from database import db_session
+from models import Record, DHT11Record
 
 
 def setUp():
@@ -13,7 +15,7 @@ def setUp():
 
 
 def create_records(pin_num):
-    with open('fixtures/dht11_data.csv') as csvfile:
+    with open('fixtures/rel_humidity_data.csv') as csvfile:
         reader = csv.reader(csvfile)
         f = reader.__next__()
         delay = dt.now().timestamp() - int(f[0])
@@ -24,6 +26,21 @@ def create_records(pin_num):
         for row in reader:
             record = Record(pin_num, int(row[2]), int(row[0]) + delay)
             db_session.add(record)
+
+        db_session.commit()
+        db_session.close()
+
+    with open('fixtures/dht11_data.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        f = reader.__next__()
+        delay = dt.now().timestamp() - int(f[0])
+
+        record = DHT11Record(int(f[1]), int(f[2]), int(f[0]) + delay)
+        db_session.add(record)
+
+        for row in reader:
+            r = DHT11Record(int(row[1]), int(row[2]), int(row[0]) + delay)
+            db_session.add(r)
 
         db_session.commit()
         db_session.close()
