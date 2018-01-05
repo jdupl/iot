d311ea78cf1f192342cc326ecf61e4d913e07a3b
Justin Duplessis <drfoliberg@gmail.com>
2018-01-05 12:23:53 -0500

    show relay names

--- relays/manage.py
+++ relays/manage.py
@@ -10,13 +10,12 @@ from flask_migrate import Migrate, MigrateCommand
 
 from server.gpio import OPiGPIOWrapper, RPiGPIOWrapper, GPIOPrintWrapper
 from server import app, db, launch_api
-# from server import setup, launch_api
 from server.models import Pin, Schedule
 
 
 global child_processes, GPIO
 child_processes = []
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 platform_resolver = {
@@ -43,13 +42,58 @@ def drop_db():
 
 @manager.command
 def create_data():
-    s = Schedule(open_time_sec=5*60*60, run_for_sec=5, repeat_every=10)
-    db.session.add(s)
+    lights = Schedule(open_time_sec=5*60*60, run_for_sec=16*60*60)
+    db.session.add(lights)
     db.session.commit()
 
-    p = Pin(pin_id=1, name='L1.1')
-    p.schedule_id = s.id
+    fans = Schedule(open_time_sec=5*60*60, run_for_sec=5*60, repeat_every=60*60)
+    db.session.add(lights)
+    db.session.commit()
+
+    p = Pin(pin_id=37, name='L1.1')
+    p.schedule_id = lights.id
     db.session.add(p)
+
+    p = Pin(pin_id=35, name='L1.2')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=33, name='L1.3')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=31, name='L1.4')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=13, name='L1.5')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=16, name='L2.1')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=26, name='L2.2')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=29, name='L2.3')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=18, name='L2.4')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=12, name='L2.5')
+    p.schedule_id = lights.id
+    db.session.add(p)
+
+    p = Pin(pin_id=26, name='Ventilation')
+    p.schedule_id = fans.id
+    db.session.add(p)
+
     db.session.commit()
 
 
